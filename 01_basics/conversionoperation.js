4809241d0554e4e3754ad3c05b2a15985da454b3
amittomarrrr <amittomar9761@gmail.com>
2026-08-20 21:36:10 +0000

    conversion of data types

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,10 @@
+let score = "12asd"
+console.log(typeof score);
+
+let Isnumber = Number(score); // it is like typecasting to change the type of the variable , if i need all output in the form of numbers then we use it
+
+
+console.log(typeof Isnumber);
+console.log(Isnumber);  // the output is NaN it means not a number , it is an error in js because of it we can typecaste any variable. 
+
+
