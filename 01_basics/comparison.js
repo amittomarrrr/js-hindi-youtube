f27839df59326555b88d1ca877bbb30b92c546ed
amittomarrrr <amittomar9761@gmail.com>
2026-08-21 19:19:18 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,16 @@
+console.log( 3 > 1);
+console.log( 3 < 1);
+console.log( 3 >= 1);
+console.log( 3 <= 1);
+console.log( 3 == 1);
+console.log( 3 != 1);
+console.log( "3" > 1);
+
+//little bit confusing 
+console.log( null> 0);
+console.log( null>= 0); // kaise true kaise aaya 
+console.log( null==0);
+
+// === for strict check
+console.log( "3" == 3); // shows true
+console.log( "3" === 1); // shows false
